33e433cf6c832785f6e503d93a127d8c38a4859c
sergio <sopekof@gmail.com>
2024-01-27 18:39:57 -0300

    Actualizscion 27/01/24

---
+++ Funcionfind.py
@@ -0,0 +1,17 @@
+# La función find en Python se utiliza para 
+# buscar la posición de una subcadena
+# dentro de otra cadena.
+# Aquí tienes un ejemplo:
+
+frase = "Hola, soy Sope"
+posicion = frase.find("soy")
+
+print(posicion)
+
+# Este código imprimirá el valor 7, ya que la
+# subcadena "soy" comienza en la posición
+# 7 de la cadena "Hola, soy Sope".
+# Si la subcadena no se encuentra, find
+# devuelve -1. Es importante mencionar que la 
+# búsqueda es sensible a mayúsculas y 
+# minúsculas.
