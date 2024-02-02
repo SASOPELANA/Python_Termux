0cb023fa2f63e7bbb6e51ff4cc846f9795584639
sergio <sopekof@gmail.com>
2024-02-01 23:44:44 -0300

    falta menos

---
+++ Sololearn/fun1.py
@@ -0,0 +1,8 @@
+def double(nombre):
+    print(nombre * 2)
+
+double(6)
+
+def sope(macho):
+    print(macho * 2)
+sope("El macho. ")
---
+++ Sololearn/funcion2.py
@@ -0,0 +1,6 @@
+def personal_greet(name):
+    print("hello", name)
+    print("que tal tu dia.")
+
+personal_greet("juan")
+    
